5c25f76f2f81e068c70a4e1d136516a3d7f7cad8
Justin Veach <justinwveach@gmail.com>
2018-03-09 13:01:37 -0600

    Fixed bug with image reziing

--- src/config.copy.js
+++ src/config.copy.js
@@ -1,6 +1,56 @@
-export default {
+const dev = {
+  s3: {
+    BUCKET: ""
+  },
+  apiGateway: {
+    REGION: "",
+    URL: ""
+  },
   cognito: {
     USER_POOL_ID: "",
-    APP_CLIENT_ID: ""
+    APP_CLIENT_ID: "",
+    REGION: "",
+    IDENTITY_POOL_ID: ""
+  }
+};
+
+const prod = {
+  s3: {
+    BUCKET: ""
+  },
+  apiGateway: {
+    URL: "",
+    REGION: ""
+  },
+  cognito: {
+    USER_POOL_ID: "",
+    APP_CLIENT_ID: "",
+    REGION: "",
+    IDENTITY_POOL_ID: ""
+  }
+};
+
+const config = process.env.REACT_APP_STAGE === "production"
+  ? prod
+  : dev;
+
+
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
+  smImageConfig: {
+    quality: 0.75,
+    maxWidth: 800,
+    maxHeight: 2000
+  },
+  mdImageConfig: {
+    quality: 0.75,
+    maxWidth: 1200,
+    maxHeight: 2000
+  },
+  lgImageConfig: {
+    quality: .75,
+    maxWidth: 1920,
+    maxHeight: 2000
   }
 };
